2751b2e1d89e21ac0a280ce04a95102cb53efda3
VanshikaTobor <vanshika.pandey@tobor.in>
2026-04-20 17:42:22 +0530

    FONT

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Raleway } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const raleway = Raleway({
   variable: "--font-raleway",
@@ -35,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} ${raleway.variable} h-full antialiased`}
+      className={`${raleway.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-slate-50">{children}</body>
     </html>
